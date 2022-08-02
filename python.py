a9aabb88b7e2bdc20c76badfadf402a205319980
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-08-01 21:53:07 -0400

    Python Cheatsheet.

---
+++ python.py
@@ -0,0 +1,33 @@
+#PYTHON CHEATSHEET
+
+"""
+#Math Operators
+1. Addition == +
+2. Substraction == -
+3. Multiplication == *
+4. Division == /
+5. Integer Division == //
+6. Modulus == %
+7. Exponent == **
+
+#Aumented Assigment Operators
+1. var += n == var + n
+2. var -= n == var - n
+3. var *= n == var * n
+4. var /= n == var / n
+5. var %= n == var % n
+
+#Data Types
+1. Integers == 1,2,3,4,5,6
+2. Floating Numbers == 0.1,0.2,0.3,0.4,0.5,0.6
+3. Strings == 'a','A','b','B','c','C'
+4. Booleans == True/False
+
+#Naming Variables
+1. var = 'hello world'
+2. my_var = 'hello world'
+3. var2 = 'hello world'
+4. my_var3 = 'hello world'
+5. 1var #can't begin with a number
+6. _var #shouldn't be used again in the code
+"""
