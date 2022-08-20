80b3429c55cdd624724ae209905fa8eef3754b5e
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-08-20 12:29:31 -0400

    Matplotlib Cheatsheet.

--- matplotlib.py
+++ matplotlib.py
@@ -1,7 +1,36 @@
 #MATPLOTLIB CHEATSHEET
 
-"""
 #Import the library
 import matplotlib.pyplot as plt
 
-"""
+#Types of plots
+1. plt.plot #linear plot
+2. plt.plot.bar #bar plot
+3. plt.plot.scatter #scatter plot
+4. plt.plot.imshow
+5. plt.plot.contour
+6. plt.plot.pcolormesh
+7. plt.plot.quiver
+8. plt.plot.pie
+9. plt.plot.text
+10. plt.plot.fill
+11. plt.plot.step
+12. plt.plot.boxplot
+13. plt.plot.errorbar
+14. plt.plot.hist
+15. plt.plot.violinplot
+16. plt.plot.barbs
+17. plt.plot.eventplot
+18. plt.plot.hexbin
+
+#Define X Label
+plt.xlabel
+
+#Define Y Label
+plt.ylabel
+
+#Display a plot
+plt.show()
+
+#Save a plot to pdf format
+plt.savefig()
