b7c6beeb065ac5d16612ac1b8d14a6591f66df50
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-08-21 15:52:02 -0400

    Django Cheatsheet.

--- django.py
+++ django.py
@@ -16,9 +16,29 @@ automatically, so you can focus only on building the app. """
 #Create a project
 django-admin startproject {project_name}
 
+#Create an app
+django-admin startapp {app_name}
+
 #Make migrations
 python manage.py migrate #go inside your project directory to migrate
 
 #Run server
 python manage.py runserver #it runs the server on your localhost http://127.0.0.1:8000/
 
+#URLs
+urlpatters = [
+  path('{template}/', views.{template}, name='template'),
+  path('{template}/', views.{template}, name='template'),
+  path('{template}/', views.{template}, name='template')
+]
+
+#Views
+def {template}(request):
+  return render(request, '{template}.html', {})
+        ###and/or###
+def {template}(request):
+  action = ###
+  context = {
+    'action':action
+  }
+  return render(request, '{template}.html', context)
