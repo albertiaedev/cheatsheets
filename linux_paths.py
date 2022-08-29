f37774d7ad1480f63bf8df052bfe5c7775f765dd
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-08-29 18:40:42 -0400

    Linux Paths Cheatsheet.

---
+++ linux_paths.py
@@ -0,0 +1,31 @@
+#LINUX PATHS CHEATSHEET
+
+1. /bin #user binaries
+
+2. /sbn #system binaries
+
+3. /etc #configuration files
+
+4. /dev #device files
+
+5. /proc #process information
+
+6. /var #variable files
+
+7. /tmp #temporary files
+
+8. /usr #user sys resources
+
+9. /home #home directories
+
+10. /boot #boot loader files
+
+11. /lib #system libraries
+
+12. /opt #optional add on apps
+
+13. /mnt #mount directory
+
+14. /media #removable devices
+
+15. /srv #service data
