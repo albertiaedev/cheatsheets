281e90014a88689905c9bea2331555ac81a4d8d4
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-07 18:46:10 -0400

    SQL Cheatsheet.

--- sql.py
+++ sql.py
@@ -3,4 +3,17 @@
 #Select specific entries
 query = SELECT col FROM `folder.db.table`
         WHERE col='row'
-
+        #if you want to select multiple columns, you can separate them with a comma ',' between the names
+        #if you want to select all columns, you can use a '*' instead of a column name
+        
+#Count the entries in a column
+COUNT()
+#Put together all columns with the same value
+GROUP BY
+#Exclude entries with a certain criteria
+HAVING
+#Example:
+SELECT col, COUNT(ID)
+FROM `folder.db.table`
+GROUP BY col
+HAVING COUNT(ID)=n
