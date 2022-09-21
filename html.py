be638c2cdb803d12e24cc80eb2029b94913c0591
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-09-21 15:14:14 -0400

    HTML Cheatsheet.

--- html.py
+++ html.py
@@ -12,3 +12,23 @@
 
   </body>
 </html>
+
+#html tags
+
+<!-- --> #defines a comment
+<!DOCTYPE> #defines the document type
+<a> #defines a hyperlink
+<abbr> #defines an abbreviation or an acronym
+<address> #defines contact information for the author/owner of a document
+<area> #defines an area inside an image map
+<article> #defines an article
+<aside> #defines content aside from the content page
+<audio> #defines embedded sound content
+<b> #defines bold text
+<base> #specifies the base URL/target for all relative URLs in a document
+<bdi> #isolates a part of text that might be formatted in a different direction from other text outside it
+<bdo> #overrides the current text direction
+<blockquote> #defines a section that is quoted from another source
+<body> #defines the documents body
+<br> #defines a single line break
+<button> #defines a clickable button
