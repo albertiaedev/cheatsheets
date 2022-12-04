71738424f68b3cc8cd82b343c264e4782c5dff96
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-12-04 12:22:26 -0400

    HTML Cheatsheet.

---
+++ html.js
@@ -0,0 +1,60 @@
+//HTML CHEATSHEET
+
+//html boiler-plate
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
+
+//html tags
+
+<!-- --> // defines a comment
+<!DOCTYPE> // defines the document type
+<a> // defines a hyperlink
+<abbr> // defines an abbreviation or an acronym
+<address> // defines contact information for the author/owner of a document
+<area> // defines an area inside an image map
+<article> // defines an article
+<aside> // defines content aside from the content page
+<audio> // defines embedded sound content
+<b> // defines bold text
+<base> // specifies the base URL/target for all relative URLs in a document
+<bdi> // isolates a part of text that might be formatted in a different direction from other text outside it
+<bdo> // overrides the current text direction
+<blockquote> // defines a section that is quoted from another source
+<body> // defines the documents body
+<br> // defines a single line break
+<button> // defines a clickable button
+<canvas> // used for drawing graphics, on the fly, with a script (usually JavaScript)
+<caption> // defines a table caption
+<cite> // defines the title of the work
+<code> // defines a piece of computer code
+<col> // specifies column properties for each column within a <colgroup> element
+<colgroup> // specifies a group of one or more colums in a table for formatting
+<data> // adds a machine-readable translation of a given content
+<datalist> // specifies a list of pre-defined options for input controls
+<dd> // defines a description/value of a term in a description list
+<del> // defines text that has been deleted from a document
+<details> // defines additional details that the user can view or hide
+<dfn> // specifies a term that is going to be defined within the content
+<dialog> // defines a dialog box or window
+<div> // defines a section in a document
+<dl> // defines a description list
+<dt> // defines a term/name in a description list
+<em> // defines emphasized text
+<embed> // defines a container for an external application
+<fieldset> // groups related elements in a form
+<footer> // defines a footer for a document or section
+<form> // defines a HTML form for user input
+<h1>-<h6> // defines HTML headings
+<head> // contains metadata/information for the document
+<header> // defines a header for a document or section
+<hr> // defines a thematic change in content
+<html> // defines the root of a HTML document
