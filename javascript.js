c8a95401867341e5dd7657d54b5a04d0e557c546
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-12-04 12:10:33 -0400

    JavaScript Cheatsheet.

---
+++ javascript.js
@@ -0,0 +1,12 @@
+//JAVASCRIPT CHEATSHEET
+
+//String Methods
+1. string.includes('substring'); // checks whether a substring exists inside of a string
+2. string.indexOf(n); /*returns the index of the first occurrence of the specified value,
+                        starting the search at fromIndex.
+                        returns -1 if the value is not found.*/
+3. string.lastIndexOf(n); /*returns the index of the last occurrence of the specified value,
+                            searching backwards from fromIndex.
+                            returns -1 if the value is not found.*/
+4. string.slice(beginIndex, endIndex); /*extracts a section of a string and returns it as a new
+                                         string, without modifying the original string.*/
