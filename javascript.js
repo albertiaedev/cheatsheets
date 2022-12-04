f7027ac57ede4929c45292be1a75268e0ddbcc41
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-12-04 17:10:38 -0400

    JavaScript Cheatsheet.

--- javascript.js
+++ javascript.js
@@ -10,3 +10,17 @@
                             returns -1 if the value is not found.*/
 4. string.slice(beginIndex, endIndex); /*extracts a section of a string and returns it as a new
                                          string, without modifying the original string.*/
+
+//Array Methods
+1. {nameOfArray}[n]; // returns a certain value from an array
+2. push(value); // adds the value to the end of the array
+3. pop(); // removes the value from the end of the array
+4. shift(); // removes the value from the start of the array
+5. unshift(value); // adds the value to the start of the array
+6. splice(fromIndex, no_of_elements); // removes the number_of_elements, starting from fromIndex from the array
+7. slice(fromIndex, toIndex); // copies a certain part of the array
+8. concat(); // join several arrays into one
+9. join(''); // returns a string of array values
+10. array.length; // returns the number of elements in the array
+11. reverse(); // reverse the order of the elements in an array
+12. toString(); // returns a string representing the specified array and its elements
