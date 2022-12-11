f1d417ed8c1c66a64d3805365333ae58f5c37734
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-12-10 23:00:22 -0400

    HTTP Protocols Cheatsheet.

--- http.py
+++ http.py
@@ -29,3 +29,53 @@
 306 #switch proxy
 307 #temporary redirect
 308 #permanent redirect
+
+#HTTP Client Error Codes
+400 #bad request
+401 #unauthorized
+402 #payment required
+403 #forbidden
+404 #not found
+405 #method not allowed
+406 #not acceptable 
+407 #proxy authentication required
+408 #request timeout
+409 #conflict
+410 #gone
+411 #length required
+412 #precondition failed
+413 #request entity too large
+414 #request-url too long
+415 #unsupported media type
+416 #requested range not satifiable
+417 #expectation failed
+418 #i'm a teapot
+422 #unprocessable entity
+423 #locked
+424 #failed dependency
+425 #unordered collection
+426 #upgrade required
+428 #precondition required
+429 #too many requests
+431 #request header fields too large
+444 #no response
+449 #retry with
+450 #blocked by windows parental controls
+451 #unavailable for legal reasons
+499 #client closed request
+
+#HTTP Server Error Codes
+500 #internal server error
+501 #not implemented
+502 #bad gateway
+503 #service unavailable
+504 #gateway timeout
+505 #http version not supported
+506 #variant also negotiates
+507 #insufficient storage
+508 #loop detected
+509 #bandwidth limit exceeded
+510 #not extended
+511 #network authentication required
+598 #network read timeout error
+599 #network connect timeout error
