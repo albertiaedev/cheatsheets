eb43a08de8dc674fc50f047faa9a01ee81baff39
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-12-10 22:34:58 -0400

    HTTP Protocols Cheatsheet.

---
+++ http.py
@@ -0,0 +1,31 @@
+#HTTP PROTOCOLS
+
+#HTTP Informational Codes
+100 #continue
+101 #switching protocols
+102 #processing
+103 #checkpoint
+122 #request-url too long
+
+#HTTP Successful Codes
+200 #ok
+201 #created
+202 #accepted
+203 #non-authoritative information
+204 #no content
+205 #reset content
+206 #partial content
+207 #multi-status
+208 #already reported
+226 #IM used
+
+#HTTP Redirection Codes
+300 #multiple choices
+301 #moved permanently
+302 #found
+303 #see other
+304 #not modified
+305 #use proxy
+306 #switch proxy
+307 #temporary redirect
+308 #permanent redirect
