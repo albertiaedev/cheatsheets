46ab58963d57705a37b76fb40bca3ed45aa41c47
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-12-11 17:08:13 -0400

    TypeScript Cheatsheet.

--- typescript.ts
+++ typescript.ts
@@ -1,1 +1,40 @@
 //TYPESCRIPT CHEATSHEET
+
+// Basic Types
+any
+void
+
+boolean
+number
+string
+
+null
+undefined
+
+bigint
+symbol
+
+string[]          /* or Array<string> */
+[string, number]  /* tuple */
+
+string | null | undefined   /* union */
+
+never  /* unreachable */
+unknown
+
+enum Color {
+  Red,
+  Green,
+  Blue = 4
+};
+
+let c: Color = Color.Green
+
+// Declarations
+let isDone: boolean
+let isDone: boolean = false
+
+function add (a: number, b: number): number {
+  return a + b
+} // Return type is optional
+function add (a: number, b: number) { ... }
