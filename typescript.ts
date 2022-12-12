55b638690bde7910d7229c842fb32811a0e26d1f
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-12-11 23:21:02 -0400

    TypeScript Cheatsheet.

--- typescript.ts
+++ typescript.ts
@@ -98,3 +98,80 @@ type ColorfulCircle = Colorful & Circle;
 interface User { ... }
 function getUser(callback: (user: User) => any) { callback({...}) }
 getUser(function (user: User) { ... })
+
+//Classes
+class Point {
+  x: number
+  y: number
+  static instances = 0
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
+  }
+}
+// inheritance
+class Point {...}
+class Point3D extends Point {...}
+interface Colored {...}
+class Pixel extends Point implements Colored {...}
+
+// short fields initialization
+class Point {
+  static instances = 0;
+  constructor(
+    public x: number,
+    public y: number,
+  ){}
+}
+                               
+// fields which do not require initialization
+class Point {
+  public someUselessValue!: number;
+  ...
+}
+
+//Generics
+class Greeter<T> {
+  greeting: T
+  constructor(message: T) {
+    this.greeting = message
+  }
+}
+let greeter = new Greeter<string>('Hello, world')
+
+//Modules
+export interface User { ... }
+
+//Type extraction
+interface Building {
+  room: {
+    door: string;
+    walls: string[];
+  };
+}
+type Walls = Building['room']['walls']; // string[]
+
+//Keyof Type Operator
+type Point = { x: number; y: number };
+type P = keyof Point; // x | y
+
+//Conditional Types
+// someType extends otherType ? TrueType : FalseType;
+type ToArray<T> = T extends any ? T[] : never;
+type StrArrOrNumArr = ToArray<string | number>; // string[] | number[]
+// inferring
+type GetReturnType<T> = T extends (...args: unknown[]) => infer R
+  ? R
+  : never;
+type Num = GetReturnType<() => number>; // number
+type First<T extends Array<any>> = T extends [infer F, ...infer Rest] ? F : never;
+type Str = First<['hello', 1, false]>; // 'hello'
+
+//Literal Type
+const point = { x: 4, y: 2 }; // { x: number, y: number }
+const literalPoint = { x: 4, y: 2 } as const; // { readonly x: 4, readonly y: 2 };
+
+//Template Literal Type
+type SpaceChar = ' ' | '\n' | '\t';
+type TrimLeft<S extends string> = S extends `${SpaceChar}${infer Rest}` ? TrimLeft<Rest> : S;
+type Str = TrimLeft<'    hello'>; // 'hello'
