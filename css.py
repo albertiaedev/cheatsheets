cd04eabdc73246b07234b7b5552af5860c657569
J.A. Hernández <105027465+jesusalberto18@users.noreply.github.com>
2022-12-15 22:02:50 -0400

    CSS Cheatsheet.

--- css.py
+++ css.py
@@ -1,6 +1,31 @@
 #CSS CHEATSHEET
 
-'''
-this cheatsheet is
-coming soon
-'''
+#Positions
+1. position #sets the position
+2. float #specifies how elements should float
+3. z-index #sets the order of overlapping elements
+4. display #sets the display
+5. top #sets the position from the top
+6. left #sets the position from the left
+7. right #sets the position from the right
+8. bottom #sets the position from the bottom
+
+#Pseudo-Classes
+1. a:hover #selects links on mouse hover
+2. input:active #selects the active link
+3. a:visited #selects all visited links
+4. input:checked #selects every checked input element
+5. input:focus #selects input that has focus
+6. input:out-of-range #select input with value out of range
+7. input:read-only #select inpt with read-only attribute
+8. input:required #select input with required attribute
+
+#Selectors
+1. * #all elements
+2. #id #element with id
+3. .class #element with class
+4. div #all div containers
+5. div > p #all p tags inside a div
+6. div + p #p tag immediately after a div
+7. div ~ p #p tag preceded by a div
+8. div.intro #div tag with class intro
