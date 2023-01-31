08b33c1ba86d037d2ece058dd7c5a9041737a630
J.A. Hernández <albertiae.digital@gmail.com>
2023-01-31 18:54:28 -0400

    PHP Cheatsheet.

--- php.php
+++ php.php
@@ -3,4 +3,13 @@
 <?php
   // PHP Code
 ?>
-// A PHP file normally contains HTML tags, and some PHP scripting code
+//A PHP file normally contains HTML tags, and some PHP scripting code
+
+
+//Declaring variables
+<?php
+  $txt = "Hello world!";  //A variable starts with the $ sign, followed by the name of the variable
+  $x = 5;
+  $y = 10.5;
+?> //Variable names are case-sensitive ($age and $AGE are two different variables)
+
