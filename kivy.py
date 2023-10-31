3cddb811bad04170ac4f61c9ecd53ac513ba258d
J.A. Hernández <albertiae.digital@gmail.com>
2023-10-31 13:53:35 -0400

    Kivy Cheatsheet

--- kivy.py
+++ kivy.py
@@ -9,3 +9,12 @@ label = Label(text='Hello, Kivy!')
 
 # Add the label widget to the root widget
 root.add_widget(label)
+
+# Create a box layout
+box_layout = BoxLayout(orientation='vertical')
+
+# Add the label widget to the box layout
+box_layout.add_widget(label)
+
+# Add the box layout to the root widget
+root.add_widget(box_layout)
