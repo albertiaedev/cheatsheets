e971173ad8252733e2df50306a46bc073cdea2df
J.A. Hernández <albertiae.digital@gmail.com>
2023-10-31 13:52:15 -0400

    Kivy Cheatsheat.

--- kivy.py
+++ kivy.py
@@ -3,3 +3,9 @@
 # Imports
 from kivy.app import App
 from kivy.uix.label import Label
+
+# Create a label widget
+label = Label(text='Hello, Kivy!')
+
+# Add the label widget to the root widget
+root.add_widget(label)
