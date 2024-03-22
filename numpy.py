30e582a1570f9bb31e3ee3b39fa2ec48ba429a0a
jesusalberto18 <albertiae.digital@gmail.com>
2024-03-22 12:38:35 -0400

    NumPy Cheatsheet.

--- numpy.py
+++ numpy.py
@@ -7,3 +7,4 @@ import numpy as np
 
 1. np.array # Create a numpy array from a Python list or tuple
 2. np.arrange # Makes evenly spaced array within a given range
+3. np.zeros # Create an array filled with zeros
