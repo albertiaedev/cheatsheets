6eab2ff6c12c4acd5a936c1952103ef5fe68a969
jesusalberto18 <albertiae.digital@gmail.com>
2024-03-22 12:40:21 -0400

    NumPy Cheatsheet.

--- numpy.py
+++ numpy.py
@@ -9,3 +9,4 @@ import numpy as np
 2. np.arrange # Makes evenly spaced array within a given range
 3. np.zeros # Create an array filled with zeros
 4. np.ones # Create an array filled with ones
+5. np.linspace # Evenly spaced numbers over a specified range
