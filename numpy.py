71fe28a33d4fe319c74cc8837ccfb5f5ad25c508
jesusalberto18 <albertiae.digital@gmail.com>
2024-03-24 13:57:37 -0400

    NumPy Cheatsheet.

--- numpy.py
+++ numpy.py
@@ -5,14 +5,14 @@
 import numpy as np
 
 
-1. np.array # Create a numpy array from a Python list or tuple
-2. np.arrange # Makes evenly spaced array within a given range
-3. np.zeros # Create an array filled with zeros
-4. np.ones # Create an array filled with ones
-5. np.linspace # Evenly spaced numbers over a specified range
-6. np.reshape # Reshape an array into a new shape
-7. np.random.rand # Array of random numbers from an uniform distribution
-8. np.sum # Compute the sum of array elements
-9. np.mean # Compute the mean of array elements
-10. np.argmax # Find the index of max value in an array
-11. np.argmin # Find the index of min value in an array
+1. np.array() # Create a numpy array from a Python list or tuple
+2. np.arrange() # Makes evenly spaced array within a given range
+3. np.zeros() # Create an array filled with zeros
+4. np.ones() # Create an array filled with ones
+5. np.linspace() # Evenly spaced numbers over a specified range
+6. np.reshape() # Reshape an array into a new shape
+7. np.random.rand() # Array of random numbers from an uniform distribution
+8. np.sum() # Compute the sum of array elements
+9. np.mean() # Compute the mean of array elements
+10. np.argmax() # Find the index of max value in an array
+11. np.argmin() # Find the index of min value in an array
